fb49645ed005ecb3469d38b2c0f5ecc464032d40
Алексей Знаменский <azum23@gmail.com>
2017-07-24 15:40:26 +0300

    start

--- T12_LINQ_01/Program.cs
+++ T12_LINQ_01/Program.cs
@@ -8,8 +8,37 @@ namespace T12_LINQ_01
 {
     class Program
     {
+        static void Reflector(object result)
+        {
+            Console.WriteLine(result.GetType().Name);
+            Console.WriteLine(result.GetType().Assembly.GetName().Name);
+        }
+
+        static void Query()
+        {
+            string[] cityes = { "New York", "Arzamas 16",
+                "Saint-Petersburg", "Moscow",
+                "Chelyabinsk23", "Ozersk" };
+
+            IEnumerable<string> subset = from c in cityes
+                                         where c.Contains("a")
+                                         orderby c
+                                         select c;
+           
+            foreach(var c in subset)
+            {
+                Console.WriteLine(c);
+            }
+            Reflector(subset);
+        }
+
         static void Main(string[] args)
         {
+
+
+            Query();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ T12_LINQ_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T12_LINQ_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] first = { 1, 2, 4, 5, 6, 76, 4, 3, 4, 55, 12, 5, 4, 8, 44, 4, 3 };
+            int[] query = ((from i in first
+                           where i % 2 == 0
+                           select i).ToArray());
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
